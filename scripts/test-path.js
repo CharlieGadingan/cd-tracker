c4cb55e9b42c19c079397c423f77200f71d07ead
dexxf <dexter101306@gmail.com>
2026-03-15 11:30:19 +0800

    chore: remove backend and non-frontend project files

--- scripts/test-path.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-console.log('Current directory:', __dirname);
-console.log('Files in current directory:');
-const fs = require('fs');
-fs.readdirSync(__dirname).forEach(file => {
-  console.log(' -', file);
-});
